6b6b548906a2d8a595405cbfb37f19a9b09f6094
Roman Michailov <roma321m@gmail.com>
2023-06-14 12:07:47 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/phonetextfiledserver/PhoneTextFiledServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.phonetextfiledserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhoneTextFiledServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhoneTextFiledServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/phonetextfiledserver/PhoneTextFiledServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.phonetextfiledserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhoneTextFiledServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
